77a327cfa1d65bdcde5025bbfbd51b6711849c1b
humairasadhameed <160359474+humairasadhameed@users.noreply.github.com>
2024-11-10 23:53:59 +0500

    Configure static export for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  assetPrefix: isProd ? '/<repository-name>/' : '',
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
