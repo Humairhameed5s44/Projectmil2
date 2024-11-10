6643e8cc25a0133f4bafaf23adf32fcbe87029e8
humairasadhameed <160359474+humairasadhameed@users.noreply.github.com>
2024-11-10 05:13:43 +0500

    profile changed

--- components/Misc/OpenGraphHead.tsx
+++ components/Misc/OpenGraphHead.tsx
@@ -1,11 +1,10 @@
 import Head from 'next/head'
 const OpenGraphHead = () => (
   <Head>
-    <title>M Asad | Web Developer</title>
-    <meta property="og:title" content="Asad hameed |  jr.Dev" />
-    <meta property="og:site_name" content="M Humair Asad Hameed" />
-   
-
+    <title>M Humair | Web Developer</title>
+    <meta property="og:title" content="M Humair | Web Developer" />
+    <meta property="og:site_name" content="M Humair Asad" />
+    
   </Head>
 )
 export default OpenGraphHead
